9636a592b46c801a317493d739db43755a629142
Jordan Roberts <jrob12921@gmail.com>
2016-03-09 15:48:29 -0500

    Created actions and some validations for jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,35 @@
 class JobsController < ApplicationController
+  def index
+    @jobs = Job.all
+  end
 
   def show
     @job = Job.find(params[:id])
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    @job.update(job_params)
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+  end
+
+  private
+  def job_params
+    params.require(:job).permit(:boat_id, :ctn_need, :cargo, :origin, :cost, :destination)
   end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,8 @@
 class Job < ActiveRecord::Base
   belongs_to :boat
+
+  CITIES = ["New York", "Los Angeles", "Washington", "Portland", "Lisbon", "London", "Amsterdam", "Cape Town", "Oslo", "Sydney", "Buenos Aires", "Lima"]
+
+  validates :origin, inclusion: {in: CITIES, "Origin is not a valid destination."}
+  validates :destination, inclusion: {in: CITIES, message: "Destination is not a valid city."}
 end
